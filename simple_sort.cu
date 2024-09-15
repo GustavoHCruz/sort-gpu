d147fbecdad6024449b1837c84baff27410f411e
Gustavo Henrique Ferreira Cruz <gustavohferreiracruz@gmail.com>
2024-09-15 17:24:01 -0300

    fixes

--- simple_sort.cu
+++ simple_sort.cu
@@ -1,78 +1,56 @@
 // Discente: Gustavo Henrique Ferreira Cruz
+#include <cooperative_groups.h>
 #include <cuda_runtime.h>
 #include <thrust/device_vector.h>
 #include <thrust/sort.h>
 #include <stdio.h>
-#include "bitonic_sort.h"
+#include <limits.h>
+
+#define CUDA_CHECK(call)                                               \
+  {                                                                    \
+    cudaError_t err = call;                                            \
+    if (err != cudaSuccess)                                            \
+    {                                                                  \
+      fprintf(stderr, "CUDA error at %s:%d: %s\n", __FILE__, __LINE__, \
+              cudaGetErrorString(err));                                \
+      exit(EXIT_FAILURE);                                              \
+    }                                                                  \
+  }
 
 #define THREADS_PER_BLOCK 1024
-#define MAX_RAND 10000
+#define SHARED_SIZE_LIMIT 1024U
 
-__global__ void partition_insert(uint *vertical_scan, uint *global_scan, uint n_histograms, uint *input, uint *output, uint n_elements, uint smallest, uint histogram_factor)
+namespace cg = cooperative_groups;
+
+__global__ void blockAndGlobalHisto(uint *input, uint n_elements, uint *global_histogram, uint *line_histogram, uint histogram_factor, uint n_histograms, uint smallest)
 {
-  extern __shared__ uint hist_counter[];
+  extern __shared__ uint local_histogram[];
 
   uint thread_id = threadIdx.x;
   uint block_id = blockIdx.x;
-
-  uint curr_line_index_mult = block_id * n_histograms;
-  uint curr_input_offset_mult = block_id * blockDim.x;
-
-  if (thread_id < n_histograms)
-    hist_counter[thread_id] = vertical_scan[thread_id + curr_line_index_mult] + global_scan[thread_id];
-
-  __syncthreads();
-
-  if (thread_id + curr_input_offset_mult < n_elements)
-  {
-    uint input_value = input[thread_id + curr_input_offset_mult];
-    uint interval_to_insert = min((input_value - smallest) / histogram_factor, n_histograms - 1);
-    uint position_to_insert = atomicAdd(&hist_counter[interval_to_insert], 1);
-
-    output[position_to_insert] = input_value;
-  }
-
-  __syncthreads();
-}
-
-__global__ void vertical_scan(uint *line_histogram, uint *vertical_scan, uint n_histograms, uint line_amount)
-{
-  uint thread_id = threadIdx.x;
-  uint col = blockIdx.x;
-  uint line = n_histograms;
+  uint input_index = thread_id + block_id * blockDim.x;
 
   if (thread_id == 0)
-    vertical_scan[col] = 0;
+    for (uint i = 0; i < n_histograms; i++)
+      local_histogram[i] = 0;
+
   __syncthreads();
 
-  if (thread_id < line_amount)
+  if (input_index < n_elements)
   {
-    vertical_scan[col + (line * (thread_id + 1))] = line_histogram[col + (line * thread_id)];
-    __syncthreads();
+    uint histogram_position = min((input[input_index] - smallest) / histogram_factor, n_histograms - 1);
 
-    for (uint stride = 1; stride < line_amount; stride *= 2)
-    {
-      uint index = (thread_id + 1) * stride * 2 - 1;
-      if (index < line_amount)
-        vertical_scan[col + (line * index)] += vertical_scan[col + (line * (index - stride))];
-
-      __syncthreads();
-    }
-
-    for (uint stride = blockDim.x; stride > 0; stride /= 2)
-    {
-      __syncthreads();
-      uint index = (thread_id + 1) * stride * 2 - 1;
-      if (index + stride < line_amount)
-        vertical_scan[col + (line * (index + stride))] += vertical_scan[col + (line * index)];
-    }
-    __syncthreads();
-
-    vertical_scan[col + (line * thread_id)] = vertical_scan[col + (line * thread_id)];
+    atomicAdd(&global_histogram[histogram_position], 1);
+    atomicAdd(&local_histogram[histogram_position], 1);
   }
+
+  __syncthreads();
+
+  if (thread_id == 0)
+    memcpy(line_histogram + (block_id * n_histograms), local_histogram, n_histograms * sizeof(uint));
 }
 
-__global__ void prefix_sum(uint *global_histogram, uint *global_histogram_scan, uint n_histograms)
+__global__ void globalHistoScan(uint *global_histogram, uint *global_histogram_scan, uint n_histograms)
 {
   extern __shared__ uint scan[];
 
@@ -110,53 +88,147 @@ __global__ void prefix_sum(uint *global_histogram, uint *global_histogram_scan, 
   }
 }
 
-__global__ void block_histogram(uint *input, uint n_elements, uint *global_histogram, uint *line_histogram, uint histogram_factor, uint n_histograms, uint smallest)
+__global__ void verticalScanHH(uint *line_histogram, uint *vertical_scan, uint n_histograms, uint blocks_per_grid)
 {
-  extern __shared__ uint local_histogram[];
+  uint tid = threadIdx.x;
+
+  if (tid == 0)
+  {
+    for (uint i = 0; i < blocks_per_grid; i++)
+    {
+      for (uint j = 0; j < n_histograms; j++)
+      {
+        if (i == 0)
+        {
+          vertical_scan[j] = 0;
+        }
+        else
+        {
+          vertical_scan[i * n_histograms + j] = vertical_scan[(i - 1) * n_histograms + j] + line_histogram[(i - 1) * n_histograms + j];
+        }
+      }
+    }
+  }
+}
+
+__global__ void partitionKernel(uint *vertical_scan, uint *global_scan, uint n_histograms, uint *input, uint *output, uint n_elements, uint smallest, uint histogram_factor)
+{
+  extern __shared__ uint hist_counter[];
 
   uint thread_id = threadIdx.x;
   uint block_id = blockIdx.x;
-  uint input_index = thread_id + block_id * blockDim.x;
 
-  if (thread_id == 0)
-    for (uint i = 0; i < n_histograms; i++)
-      local_histogram[i] = 0;
+  uint curr_line_index_mult = block_id * n_histograms;
+  uint curr_input_offset_mult = block_id * blockDim.x;
+
+  if (thread_id < n_histograms)
+    hist_counter[thread_id] = vertical_scan[thread_id + curr_line_index_mult] + global_scan[thread_id];
 
   __syncthreads();
 
-  if (input_index < n_elements)
+  if (thread_id + curr_input_offset_mult < n_elements)
   {
-    uint histogram_position = min((input[input_index] - smallest) / histogram_factor, n_histograms - 1);
+    uint input_value = input[thread_id + curr_input_offset_mult];
+    uint interval_to_insert = min((input_value - smallest) / histogram_factor, n_histograms - 1);
+    uint position_to_insert = atomicAdd(&hist_counter[interval_to_insert], 1);
 
-    atomicAdd(&global_histogram[histogram_position], 1);
-    atomicAdd(&local_histogram[histogram_position], 1);
+    output[position_to_insert] = input_value;
+  }
+
+  __syncthreads();
+}
+
+__device__ inline void compareAndSwap(uint *data, uint a, uint b, uint dir)
+{
+  uint temp;
+  if ((data[a] > data[b]) == dir)
+  {
+    temp = data[a];
+    data[a] = data[b];
+    data[b] = temp;
+  }
+}
+
+__global__ void blockBitonicSort(uint *global_histogram_scan, uint *output, uint n_histograms, uint n_elements)
+{
+  cg::thread_block cta = cg::this_thread_block();
+
+  uint block_id = blockIdx.x;
+  uint block_dim = blockDim.x;
+  uint thread_id = threadIdx.x;
+
+  __shared__ uint aux[SHARED_SIZE_LIMIT];
+
+  uint start_index = global_histogram_scan[block_id];
+  uint end_index = n_elements;
+  if (block_id + 1 < n_histograms)
+    end_index = global_histogram_scan[block_id + 1];
+
+  if (start_index + thread_id < end_index)
+    aux[thread_id] = output[start_index + thread_id];
+  else
+    aux[thread_id] = UINT_MAX;
+
+  __syncthreads();
+
+  uint dir = 1;
+
+  for (uint size = 2; size < block_dim; size <<= 1)
+  {
+    // Bitonic merge
+    uint ddd = dir ^ ((threadIdx.x & (size / 2)) != 0);
+
+    for (uint stride = size / 2; stride > 0; stride >>= 1)
+    {
+      cg::sync(cta);
+      uint pos = 2 * threadIdx.x - (threadIdx.x & (stride - 1));
+      if (pos + stride < block_dim)
+        compareAndSwap(aux, pos, pos + stride, ddd);
+    }
+  }
+
+  {
+    for (uint stride = block_dim / 2; stride > 0; stride >>= 1)
+    {
+      cg::sync(cta);
+      uint pos = 2 * threadIdx.x - (threadIdx.x & (stride - 1));
+      if (pos + stride < block_dim)
+        compareAndSwap(aux, pos, pos + stride, dir);
+    }
   }
 
   __syncthreads();
 
-  if (thread_id == 0)
-    memcpy(line_histogram + (block_id * n_histograms), local_histogram, n_histograms * sizeof(uint));
+  if (start_index + thread_id < end_index)
+    output[start_index + thread_id] = aux[thread_id];
 }
 
 bool verify_sort(uint *a, uint *b, uint size)
 {
   bool flag = true;
-  for (int i = 0; i < size; i++)
+  for (uint i = 0; i < size; i++)
     if (a[i] != b[i])
+    {
       flag = false;
+      printf("Mismatch at [%u]: %u X %u\n", i, a[i], b[i]);
+    }
 
   return flag;
 }
 
 void initialize_input_vector(uint *input, uint n_elements, uint *smallest, uint *biggest)
 {
-  *smallest = std::numeric_limits<uint>::max();
-  *biggest = std::numeric_limits<uint>::min();
+  *smallest = UINT_MAX;
+  *biggest = 0;
 
   for (uint i = 0; i < n_elements; i++)
   {
-    int a = rand() % (MAX_RAND + 1);
-    uint v = static_cast<uint>(a);
+    int a = rand();
+    int b = rand();
+
+    int c = a * 20 * b;
+
+    uint v = static_cast<uint>(c);
 
     if (v > *biggest)
       *biggest = v;
@@ -212,69 +284,66 @@ int main(int argc, char *argv[])
   printf("Histogram Value Range Width:%u\n", histogram_factor);
   // ------------------------------ Prints
 
+  cudaMalloc(&d_input, input_size);
+  cudaMalloc(&d_output, input_size);
+  cudaMalloc(&d_global_histogram, histogram_size);
+  cudaMemset(d_global_histogram, 0, histogram_size);
+  cudaMalloc(&d_global_histogram_scan, histogram_size);
+
+  cudaMalloc(&d_line_histogram, blocks_per_grid * histogram_size);
+  cudaMemset(d_line_histogram, 0, blocks_per_grid * histogram_size);
+  cudaMalloc(&d_vertical_scan, blocks_per_grid * histogram_size);
+  cudaMalloc(&d_aux, max_size_aux * sizeof(uint));
+
+  cudaMemcpy(d_input, h_input, input_size, cudaMemcpyHostToDevice);
+
   cudaEventRecord(start);
   for (uint r = 0; r < n_repetitions; r++)
   {
-    cudaMalloc(&d_input, input_size);
-    cudaMalloc(&d_output, input_size);
-    cudaMalloc(&d_global_histogram, histogram_size);
-    cudaMemset(d_global_histogram, 0, histogram_size);
-    cudaMalloc(&d_global_histogram_scan, histogram_size);
+    blockAndGlobalHisto<<<blocks_per_grid, THREADS_PER_BLOCK, histogram_size>>>(d_input, n_elements, d_global_histogram, d_line_histogram, histogram_factor, n_histograms, smallest);
+    CUDA_CHECK(cudaGetLastError());
+    CUDA_CHECK(cudaDeviceSynchronize());
 
-    cudaMalloc(&d_line_histogram, blocks_per_grid * histogram_size);
-    cudaMemset(d_line_histogram, 0, blocks_per_grid * histogram_size);
-    cudaMalloc(&d_vertical_scan, blocks_per_grid * histogram_size);
-    cudaMalloc(&d_aux, max_size_aux * sizeof(uint));
+    printf("Execução 1 finalizada\n");
 
-    cudaMemcpy(d_input, h_input, input_size, cudaMemcpyHostToDevice);
-    block_histogram<<<blocks_per_grid, THREADS_PER_BLOCK, histogram_size>>>(d_input, n_elements, d_global_histogram, d_line_histogram, histogram_factor, n_histograms, smallest);
-    cudaDeviceSynchronize();
+    globalHistoScan<<<1, THREADS_PER_BLOCK, histogram_size>>>(d_global_histogram, d_global_histogram_scan, n_histograms);
+    CUDA_CHECK(cudaGetLastError());
+    CUDA_CHECK(cudaDeviceSynchronize());
 
-    prefix_sum<<<1, THREADS_PER_BLOCK, histogram_size>>>(d_global_histogram, d_global_histogram_scan, n_histograms);
-    cudaMemcpy(h_global_histogram_scan, d_global_histogram_scan, histogram_size, cudaMemcpyDeviceToHost);
-    cudaDeviceSynchronize();
+    printf("Execução 2 finalizada\n");
 
-    vertical_scan<<<n_histograms, THREADS_PER_BLOCK>>>(d_line_histogram, d_vertical_scan, n_histograms, blocks_per_grid);
-    cudaDeviceSynchronize();
+    verticalScanHH<<<1, THREADS_PER_BLOCK>>>(d_line_histogram, d_vertical_scan, n_histograms, blocks_per_grid);
+    CUDA_CHECK(cudaGetLastError());
+    CUDA_CHECK(cudaDeviceSynchronize());
 
-    partition_insert<<<blocks_per_grid, THREADS_PER_BLOCK, n_histograms>>>(d_vertical_scan, d_global_histogram_scan, n_histograms, d_input, d_output, n_elements, smallest, histogram_factor);
-    cudaMemcpy(h_output, d_output, input_size, cudaMemcpyDeviceToHost);
-    cudaDeviceSynchronize();
+    printf("Execução 3 finalizada\n");
 
-    for (uint i = 0; i < n_histograms; i++)
-    {
-      uint start_index = h_global_histogram_scan[i];
-      uint end_index = n_elements;
-      if (i + 1 < n_histograms)
-        end_index = h_global_histogram_scan[i + 1];
+    partitionKernel<<<blocks_per_grid, THREADS_PER_BLOCK, histogram_size>>>(d_vertical_scan, d_global_histogram_scan, n_histograms, d_input, d_output, n_elements, smallest, histogram_factor);
+    CUDA_CHECK(cudaGetLastError());
+    CUDA_CHECK(cudaDeviceSynchronize());
 
-      uint k = 0;
-      for (uint j = start_index; j < end_index; j++)
-        h_aux[k++] = h_output[j];
+    printf("Execução 4 finalizada\n");
 
-      for (uint j = end_index - start_index; j < max_size_aux; j++)
-        h_aux[j] = MAX_RAND + 1;
+    blockBitonicSort<<<n_histograms, THREADS_PER_BLOCK>>>(d_global_histogram_scan, d_output, n_histograms, n_elements);
+    CUDA_CHECK(cudaGetLastError());
+    CUDA_CHECK(cudaDeviceSynchronize());
 
-      cudaMemcpy(d_aux, h_aux, max_size_aux * sizeof(uint), cudaMemcpyHostToDevice);
-      bitonicSort(d_aux, d_aux, d_aux, d_aux, 1, max_size_aux, 1);
-      cudaDeviceSynchronize();
-      cudaMemcpy(h_aux, d_aux, max_size_aux * sizeof(uint), cudaMemcpyDeviceToHost);
-
-      k = 0;
-      for (uint j = start_index; j < end_index; j++)
-        h_output[j] = h_aux[k++];
-    }
-
-    cudaFree(d_input);
-    cudaFree(d_output);
-    cudaFree(d_global_histogram);
-    cudaFree(d_global_histogram_scan);
-    cudaFree(d_line_histogram);
-    cudaFree(d_vertical_scan);
-    cudaFree(d_aux);
+    printf("Execução 5 finalizada\n");
   }
   cudaEventRecord(stop);
   cudaEventSynchronize(stop);
+
+  CUDA_CHECK(cudaGetLastError());
+  CUDA_CHECK(cudaDeviceSynchronize());
+  cudaMemcpy(h_output, d_output, input_size, cudaMemcpyDeviceToHost);
+
+  cudaFree(d_input);
+  cudaFree(d_output);
+  cudaFree(d_global_histogram);
+  cudaFree(d_global_histogram_scan);
+  cudaFree(d_line_histogram);
+  cudaFree(d_vertical_scan);
+  cudaFree(d_aux);
 
   float simple_sort_milliseconds = 0;
   cudaEventElapsedTime(&simple_sort_milliseconds, start, stop);
